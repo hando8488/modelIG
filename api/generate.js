345d14e80e452b9507cb0c3bc3f614c359919ebd
Hannes Döring <hannes.doering@gmx.de>
2025-09-22 23:06:26 +0200

    update

--- api/generate.js
+++ api/generate.js
@@ -22,7 +22,7 @@ export default async function handler(req, res) {
         }
 
         const genAI = new GoogleGenerativeAI(apiKey);
-        const model = genAI.getGenerativeModel({ model: 'gemini-pro-vision' });
+        const model = genAI.getGenerativeModel({ model: 'gemini-1.5-pro-vision-latest' });
 
         const body = await req.json();
         const { prompt, rotation, images } = body;
